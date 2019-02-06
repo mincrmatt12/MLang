a3b4d150f95574b36c81e2bcd14a7f5980eb563c
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2019-02-06 00:11:22 -0500

    Semi-functioning register function tihng

--- src/codegen_64.cpp
+++ src/codegen_64.cpp
@@ -880,6 +880,104 @@ use_mem:
 	}
 
 	std::string codegenerator::assemble_special(statement *s, int label) {
-		return "; fcall\n";
+		std::string result;
+		auto emit = emitter(result);
+		if (si_fcall(*s)) {
+			// Alright: function calls.
+			storage target = get_storage_for(s->lhs());
+
+			bool added_junk = false;
+			
+			// First, push all of the caller-saved registers that we use.
+			for (int i = 0; i < 14; ++i) {
+				if (i == 7 || i > 9) continue;
+
+				if (target.type == storage::REG && target.regno == i) continue; // ignore the result variable
+				if (!is_register_used(i)) continue;
+
+				emit("push ", storage{i, 64});
+				added_junk = !added_junk;
+			}
+
+
+			// Now, make the stack line up.
+			int stack_add_amount = (16 - ((local_stack_usage + added_junk * 8) % 16)) % 16; // align the stack to 16-byte boundaries
+
+			if (stack_add_amount) emit("sub rsp, ", stack_add_amount);
+
+			// Check if the target has varargs.
+			const auto &call_tgt = *(++s->params.begin());
+			if (ai_ident(call_tgt) && call_tgt.ident.type == id_type::extern_function && ext_list[call_tgt.ident.index].varargs) {
+				// Set al to 0
+				emit("mov al, 0");
+			}
+
+			// Check if we need to use the special method for handling registers
+			std::set<int> used_so_far;
+			bool alright = true;
+			for (int i = 2; i < s->params.size(); ++i) {
+				storage s_cur = get_storage_for(s->params[i]);
+				if (s_cur.type == storage::REG && s_cur.regno == i - 2) continue; // already in the right place.
+				else {
+					// We need to move this value in.
+					int would_be = i - 2;
+					if (s_cur.type == storage::REG && used_so_far.count(s_cur.regno)) {
+						alright = false;
+						break;
+					}
+					used_so_far.insert(would_be);
+				}
+			}
+			
+			if (alright) {
+				// Move params in
+				for (int i = 2; i < s->params.size(); ++i) {
+					storage s_cur = get_storage_for(s->params[i]);
+					if (s_cur.type == storage::REG && s_cur.regno == i - 2) continue; // already in the right place.
+					else {
+						// We need to move this value in.
+						emit("mov ", storage{i - 2, s_cur.size}, ", ", s_cur);
+					}
+				}
+			}
+			else {
+				// Use the stack to cheat complex logic.
+				for (int i = 2; i < s->params.size(); ++i) {
+					emit("push ", storage{get_storage_for(s->params[i]), std::min((uint8_t)16, get_storage_for(s->params[i]).size)});
+				}
+				for (int i = 0, j = s->params.size() - 1; j >= 2; --j, ++i) {
+					emit("pop ", storage{static_cast<int>((s->params.size() - 3) - i), std::min((uint8_t)16, get_storage_for(s->params[j]).size)});
+				}
+			}
+
+			// Call function
+			if (ai_ident(call_tgt)) {
+				emit("call [rel ", call_tgt.ident.name, "]");
+			}
+			else {
+				emit("call ", storage{get_storage_for(call_tgt), 64});
+			}
+
+			// Grab result
+			if (target.type == storage::REG && target.regno != 6) {
+				emit("mov ", target, ", ", storage{6, target.size});
+			}
+
+			// Add back the stack
+			if (stack_add_amount) emit("add rsp, ", stack_add_amount);
+
+			// Unpop everything
+			for (int i = 13; i >= 0; --i) {
+				if (i == 7 || i > 9) continue;
+
+				if (target.type == storage::REG && target.regno == i) continue; // ignore the result variable
+				if (!is_register_used(i)) continue;
+
+				emit("pop ", storage{i, 64});
+			}
+
+			return result;
+		}
+		return "; unknown\n";
 	} 
 }
